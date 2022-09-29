23f69bcc7ff8ec69381717a5fadc99014ba4fc17
lostinrepo <shivanshuagnihotri2794@gmail.com>
2022-09-29 11:35:31 +0530

    DeleteView added

--- postapp/urls.py
+++ postapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from postapp.views import PostListView,PostDetailView,PostCreateView,PostUpdateView
+from postapp.views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 
 urlpatterns = [
     path("",PostListView.as_view(),name="home"),
     path("post/<int:pk>/",PostDetailView.as_view(),name="post_detail"),
     path("post/new/",PostCreateView.as_view(),name="post_new"),
     path("post/<int:pk>/edit/",PostUpdateView.as_view(),name="post_edit"),
+    path("post/<int:pk>/delete/",PostDeleteView.as_view(),name="post_delete"),
     
 ]
--- postapp/views.py
+++ postapp/views.py
@@ -1,8 +1,10 @@
 from msilib.schema import ListView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Post
 from django.views.generic import ListView,DetailView
-from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+
 
 # Create your views here.
 class PostListView(ListView):
@@ -22,5 +24,11 @@ class PostUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title","blogmessage"]
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")  #redirects user to homepage after deletion
+    
     
     
